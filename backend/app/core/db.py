98ad0c2a7e83ab1deecb26b8e9393776f5b35e30
valanm22 <valanm22@gmail.com>
2026-08-19 19:40:43 +0530

    Organize imports

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,7 +1,6 @@
+from config import settings
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
-
-from config import settings
 
 engine = create_async_engine(settings.database_url)
 
